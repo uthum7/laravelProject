7f49b2172147fa581750e70535712bc29eb05e61
uthumWijenayake <uthumwijenayake@gmail.com>
2024-12-29 18:17:58 +0530

    laravelProjectcode

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.user')
+
+@section('title', 'Home')
+
+@section('contents')
+<style>
+    body {
+        background-image: url('https://i.pinimg.com/736x/4b/c5/a8/4bc5a864bc7cb0cffbee7b40e28f7c02.jpg'); /* Replace with the actual image path */
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        height: 80vh;
+        margin: 0;
+    }
+
+    h1 {
+        font-size: 3rem;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+        margin-top: 20%;
+    }
+</style>
+
+<main>
+
+</main>
+@endsection
